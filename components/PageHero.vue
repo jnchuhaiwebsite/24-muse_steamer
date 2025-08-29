c3074c5f6dc0f324772421d78d51d4ac2e4112a6
zhangjianbin <2551490259@qq.com>
2025-08-29 14:25:53 +0800

    -

--- components/PageHero.vue
+++ components/PageHero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-center pt-28">
+  <div class="text-center mt-28">
     <h1 class="text-3xl font-extrabold tracking-tight sm:text-4xl lg:text-5xl bg-gradient-to-r from-[#6209F6] via-[#DC8AF6] to-[#83D0FB] text-transparent bg-clip-text py-2">
       <span class="block">{{ title }}</span>
     </h1>
